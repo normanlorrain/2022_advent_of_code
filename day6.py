ed82da41b306e492d387ac360b130a1393ddeab7
Norman Lorrain <normanlorrain@gmail.com>
2022-12-06 07:18:13 -0700

    Refactored.

--- day6.py
+++ day6.py
@@ -4,15 +4,18 @@ nppdvjthqldpwncqszvftbrmjlhg
 nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
 zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"""
 
-lines = test.splitlines()
-lines = open("day6.txt")
 
-numDistinct = 14   # 4, part 1.  14 part 2
-for line in lines:
-    for i in range(len(line)):
-        marker = line[i:i+numDistinct]
-        markerSet = set(marker)
-        # print(marker, len(markerSet))
-        if len(set(marker)) == numDistinct:
-            print(i+numDistinct)
-            break   
+def day( lines ):
+    numDistinct = 14   # 4, part 1.  14 part 2
+    for line in lines:
+        for i in range(len(line)):
+            marker = line[i:i+numDistinct]
+            markerSet = set(marker)
+            # print(marker, len(markerSet))
+            if len(set(marker)) == numDistinct:
+                print(i+numDistinct)
+                break   
+
+
+for lines in [test.splitlines(),open("day6.txt")]:
+    day(lines)
